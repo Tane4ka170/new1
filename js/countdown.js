d755e6f794ba573607525cde9cff0124b59c9b76
Tane4ka170 <tane4ka170@gmail.com>
2024-10-18 21:33:36 +0300

    Added refs, calc, and update logic for countdown timer

--- js/countdown.js
+++ js/countdown.js
@@ -2,13 +2,39 @@ class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.refs = {
+      days: document.querySelector(`${this.selector} [data-value = "days"]`),
+      hours: document.querySelector(`${this.selector} [data-value = "hours"]`),
+      minutes: document.querySelector(
+        `${this.selector} [data-value = "minutes"]`
+      ),
+      seconds: document.querySelector(
+        `${this.selector} [data-value = "seconds"]`
+      ),
+    };
   }
 
   getTimeRemaining(endtime) {
-    console.log(endtime);
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const mins = Math.floor((total / 1000 / 60) % 60);
+    const secs = Math.floor((total / 1000) % 60);
+    return {
+      total,
+      days,
+      hours,
+      mins,
+      secs,
+    };
   }
 
-  updateTimer({ days, hours, mins, secs }) {}
+  updateTimer({ days, hours, mins, secs }) {
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.minutes.textContent = minutes;
+    this.refs.days.textContent = days;
+  }
 
   startTimer() {
     const timer = this.getTimeRemaining(this.targetDate);

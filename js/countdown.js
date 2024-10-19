2e093315a4f4f15a25e111715add893fda4239a8
Tane4ka170 <tane4ka170@gmail.com>
2024-10-19 22:55:41 +0300

    Added color options & fixed timer update

--- js/countdown.js
+++ js/countdown.js
@@ -1,16 +1,22 @@
 class CountdownTimer {
-  constructor({ selector, targetDate }) {
+  // setup timer values
+  constructor({
+    selector,
+    targetDate,
+    backgroundColor = null,
+    foregroundColor = null,
+  }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.backgroundColor = backgroundColor;
+    this.foregroundColor = foregroundColor;
+
+    // grab divs on frontend using supplied selector ID
     this.refs = {
-      days: document.querySelector(`${this.selector} [data-value = "days"]`),
-      hours: document.querySelector(`${this.selector} [data-value = "hours"]`),
-      minutes: document.querySelector(
-        `${this.selector} [data-value = "minutes"]`
-      ),
-      seconds: document.querySelector(
-        `${this.selector} [data-value = "seconds"]`
-      ),
+      days: document.querySelector(`${this.selector} [data-value="days"]`),
+      hours: document.querySelector(`${this.selector} [data-value="hours"]`),
+      mins: document.querySelector(`${this.selector} [data-value="minutes"]`),
+      secs: document.querySelector(`${this.selector} [data-value="seconds"]`),
     };
   }
 
@@ -32,17 +38,33 @@ class CountdownTimer {
   updateTimer({ days, hours, mins, secs }) {
     this.refs.days.textContent = days;
     this.refs.hours.textContent = hours;
-    this.refs.minutes.textContent = minutes;
-    this.refs.days.textContent = days;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+  }
+
+  updateColors() {
+    if (this.backgroundColor != null) {
+      this.refs.days.style.background = this.backgroundColor;
+      this.refs.hours.style.background = this.backgroundColor;
+      this.refs.mins.style.background = this.backgroundColor;
+      this.refs.secs.style.background = this.backgroundColor;
+    }
+
+    if (this.foregroundColor != null) {
+      this.refs.days.style.color = this.foregroundColor;
+      this.refs.hours.style.color = this.foregroundColor;
+      this.refs.mins.style.color = this.foregroundColor;
+      this.refs.secs.style.color = this.foregroundColor;
+    }
   }
 
   startTimer() {
     const timer = this.getTimeRemaining(this.targetDate);
+    this.updateTimer(timer);
+    this.updateColors();
+    setInterval(() => {
+      const timer = this.getTimeRemaining(this.targetDate);
+      this.updateTimer(timer);
+    }, 1000);
   }
 }
-
-const timer1 = new CountdownTimer({
-  selector: "#clock1",
-  targetDate: new Date("July, 17 2023 16:34:26"),
-});
-timer1.startTimer();
